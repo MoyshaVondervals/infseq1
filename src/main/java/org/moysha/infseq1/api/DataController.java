e0c9742936e8fc2d7461dce14d42e5b88a56373c
blue <moyshaVondervals@gmail.com>
2026-09-16 15:05:19 +0300

    task3.2

--- src/main/java/org/moysha/infseq1/api/DataController.java
+++ src/main/java/org/moysha/infseq1/api/DataController.java
@@ -2,6 +2,7 @@ package org.moysha.infseq1.api;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.moysha.infseq1.auth.AuthFilter;
+import org.springframework.web.util.HtmlUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,9 +21,12 @@ public class DataController {
 
     @GetMapping("/data")
     public DataResponse getData(HttpServletRequest request) {
-        String username = (String) request.getAttribute(AuthFilter.USERNAME_ATTRIBUTE);
+        String username = HtmlUtils.htmlEscape(
+                (String) request.getAttribute(AuthFilter.USERNAME_ATTRIBUTE)
+        );
         List<String> items = dataItemRepository.findAllByOrderByIdAsc().stream()
                 .map(DataItem::getContent)
+                .map(HtmlUtils::htmlEscape)
                 .toList();
         return new DataResponse(username, items);
     }
